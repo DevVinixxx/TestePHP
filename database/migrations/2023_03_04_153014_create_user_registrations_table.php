de08213787085c99799689ec1ac5697f96b4a70d
Vinicius Peres <vperes505@gmail.com>
2023-03-04 12:39:42 -0300

    criada a migration de user

---
+++ database/migrations/2023_03_04_153014_create_user_registrations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_registrations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('cpf');
+            $table->datetime('birthdate');
+            $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->string('street');
+            $table->string('neighborhood');
+            $table->string('nummber');
+            $table->string('zip_code');
+            $table->string('city');
+            $table->string('state');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_registrations');
+    }
+};
