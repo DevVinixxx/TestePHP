75507b48a40e95efb24def223ee6a641e11a2d56
Vinicius Peres <vperes505@gmail.com>
2023-03-12 03:29:44 -0300

    select de cidades e estado feitas

--- resources/views/_partials/form.blade.php
+++ resources/views/_partials/form.blade.php
@@ -47,15 +47,42 @@
                     <input type="text" id="name" name="name" class="form-control" placeholder="19063841">
                 </div>
                 <div class="mb-3">
-                    <label for="state" class="form-label">Estado: </label>
+                    <label for="state" class="form-label">Selecione um state:</label>
                     <select id="state" name="state" class="form-control">
-                      <option value="#">aa</option>
+                        <option value="" disabled selected>Selecione um estado...</option>
+                        <option value="AC">Acre</option>
+                        <option value="AL">Alagoas</option>
+                        <option value="AP">Amapá</option>
+                        <option value="AM">Amazonas</option>
+                        <option value="BA">Bahia</option>
+                        <option value="CE">Ceará</option>
+                        <option value="DF">Distrito Federal</option>
+                        <option value="ES">Espírito Santo</option>
+                        <option value="GO">Goiás</option>
+                        <option value="MA">Maranhão</option>
+                        <option value="MT">Mato Grosso</option>
+                        <option value="MS">Mato Grosso do Sul</option>
+                        <option value="MG">Minas Gerais</option>
+                        <option value="PA">Pará</option>
+                        <option value="PB">Paraíba</option>
+                        <option value="PR">Paraná</option>
+                        <option value="PE">Pernambuco</option>
+                        <option value="PI">Piauí</option>
+                        <option value="RJ">Rio de Janeiro</option>
+                        <option value="RN">Rio Grande do Norte</option>
+                        <option value="RS">Rio Grande do Sul</option>
+                        <option value="RO">Rondônia</option>
+                        <option value="RR">Roraima</option>
+                        <option value="SC">Santa Catarina</option>
+                        <option value="SP">São Paulo</option>
+                        <option value="SE">Sergipe</option>
+                        <option value="TO">Tocantins</option>
                     </select>
                 </div>
                 <div class="mb-3">
-                    <label for="name" class="form-label">Cidade: </label>
-                    <select id="state" name="state" class="form-control">
-                        <option value="#">aaa</option>
+                    <label for="city" class="form-label">Selecione uma cidade:</label>
+                    <select id="city" name="city" class="form-control">
+                    //Aqui vem todas as cidades do estado selecionado
                     </select>
                 </div>
             </div>
@@ -63,3 +90,33 @@
     </div>
 </div>
 <button type="submit" class="btn btn-success mt-3">Submit</button>
+
+<script>
+    const estado = document.getElementById('state');
+    const cdd = document.getElementById('city');
+  
+    estado.addEventListener('change', (event) => {
+      const state = event.target.value;
+  
+      // faz uma requisição HTTP para a API do IBGE para obter as cidades do estado selecionado
+      fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${state}/municipios`)
+        .then(response => response.json())
+        .then(data => {
+          // remove todas as opções de cidade existentes
+          while (cdd.firstChild) {
+            cdd.removeChild(cdd.firstChild);
+          }
+  
+          // adiciona as novas opções de cidade com base na resposta da API
+          data.forEach(cidade => {
+            const option = document.createElement('option');
+            option.value = cidade.nome;
+            option.text = cidade.nome;
+            cdd.appendChild(option);
+          });
+        })
+        .catch(error => {
+          console.error(`Erro ao obter as cidades do estado ${state}: ${error.message}`);
+        });
+    });
+  </script>
